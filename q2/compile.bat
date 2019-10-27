b815b7b63644378f8b76492dfae1759667241376
Tomcat-42 <pablohuggem@gmail.com>
2019-10-27 20:16:20 -0300

    Mudado o arquivo de compilação pro windows.

--- q2/compile.bat
+++ q2/compile.bat
@@ -1,3 +1,3 @@
-cd C:\Users\<MUDA AQUI>\Desktop\trab4alg\q2\
+cd C:\Users\"Heloisa A.A"\Desktop\trab4alg\q2\
 
 gcc -Wall modulo_emprestimos\emprestimos.c modulo_livros\livros.c modulo_usuarios\usuarios.c main.c -o biblioteca.exe
