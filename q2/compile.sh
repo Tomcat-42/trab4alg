d6fb7baf398c6a1cc4539549b8bfff02cae008e1
Tomcat-42 <pablohuggem@gmail.com>
2019-10-27 01:42:38 -0300

    Adicionado algumas coisas.

---
+++ q2/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/env sh
+
+gcc -Wall ./modulo_emprestimos/emprestimos.c ./modulo_livros/livros.c ./modulo_usuarios/usuarios.c ./main.c -o biblioteca
---
+++ q2/main.c
@@ -0,0 +1,9 @@
+#include "./modulo_livros/livros.h"
+#include "./modulo_usuarios/usuarios.h"
+#include "./modulo_emprestimos/emprestimos.h"
+
+int main()
+{
+	return 0;
+}
+
